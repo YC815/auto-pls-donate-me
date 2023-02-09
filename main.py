cb45c5e34cab124b5a9f71fce3e1b9b3aeb671de
YC815 <minnecraft.YC815@gmail.com>
2023-02-09 14:59:25 +0800

    debug

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pyautogui
 import time
 # say --> x: 0334 y: 0430
 time.sleep(5)
-v
+pyautogui.moveTo(334, 430, duration = 1.5) 
+
 while True:
-    pyautogui.moveTo(334, 430, duration = 1.5) 
     pyautogui.dragTo(334, 430, duration=0.2, button="left")
     pyautogui.keyDown("shiftleft")
     pyautogui.press("p")
@@ -20,6 +20,7 @@ while True:
     pyautogui.press("e")
     pyautogui.press("enter")
     time.sleep(8)
+    pyautogui.dragTo(334, 430, duration=0.2, button="left")
     pyautogui.press("d")
     pyautogui.press("o") 
     pyautogui.press("n")
@@ -43,7 +44,9 @@ while True:
     pyautogui.press("p")
     pyautogui.press("p")
     pyautogui.press("y")
+    pyautogui.press("enter")
     time.sleep(8)
+    pyautogui.dragTo(334, 430, duration=0.2, button="left")
     pyautogui.press("p")
     pyautogui.press("l")
     pyautogui.press("s")
@@ -54,6 +57,7 @@ while True:
     pyautogui.press("s")
     pyautogui.press("s")
     pyautogui.press("s")
+    pyautogui.press("enter")
     time.sleep(8)
  
 
